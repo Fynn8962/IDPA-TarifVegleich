10e66d5e5717b1c82f62c5c94a4375324c9e38ad
Fynn Huber <fynn.huber@icloud.com>
2026-01-05 13:01:32 +0100

    deleting prototyp backend

--- Backend/src/routes/profile.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/profile.controller");
-
-router.post("/", controller.create);
-router.get("/:id", controller.getOne);
-
-module.exports = router;
